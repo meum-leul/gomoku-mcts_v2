4a64b25142dc68898eb1114e62623ef25acc29e5
박상현 <twodude@naver.com>
2017-12-02 04:15:34 +0900

    Update gameplay.py

--- untitled2/gameplay.py
+++ untitled2/gameplay.py
@@ -52,7 +52,12 @@ def play_game(player_policies):
 
 
 
-            #recommend moves : [(0, 0), (0, 1)] etc.
+            # init. values
+            saved_trees = {}
+            saved_last_move = None
+
+
+            # recommend moves : [(0, 0), (0, 1)] etc.
             recom_moves = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (0, 8), (0, 9), (0, 10), (0, 11), (0, 12), (0, 13), (0, 14), (0, 15),
                            (1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7), (1, 8), (1, 9), (1, 10), (1, 11), (1, 12), (1, 13), (1, 14), (1, 15),
                            (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7), (2, 8), (2, 9), (2, 10), (2, 11), (2, 12), (2, 13), (2, 14), (2, 15),
@@ -70,8 +75,13 @@ def play_game(player_policies):
                            (14, 0), (14, 1), (14, 2), (14, 3), (14, 4), (14, 5), (14, 6), (14, 7), (14, 8), (14, 9), (14, 10), (14, 11), (14, 12), (14, 13), (14, 14), (14, 15),
                            (15, 0), (15, 1), (15, 2), (15, 3), (15, 4), (15, 5), (15, 6), (15, 7), (15, 8), (15, 9), (15, 10), (15, 11), (15, 12), (15, 13), (15, 14), (15, 15)]
 
-            tar = player_policy.move(game, recom_moves)
+            tar, tree, last_move = player_policy.move(game, recom_moves, saved_trees, saved_last_move)
             print("MCTS result: "+str(tar))
+
+            # save these values
+            print(tree) #saved_trees
+            print(last_move) #saved_last_move
+
 
             game.move(*tar)
             print(game)
@@ -82,7 +92,6 @@ def play_game(player_policies):
             G.add_node(str(game))
             current = str(game)
             G.add_edge(previous, current)
-
 
 
 
