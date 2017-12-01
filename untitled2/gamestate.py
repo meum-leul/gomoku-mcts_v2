62230870a4c1eaac5b2c22d4012f44690e07529e
박상현 <twodude@naver.com>
2017-12-02 02:17:30 +0900

    Update gamestate.py

--- untitled2/gamestate.py
+++ untitled2/gamestate.py
@@ -1,32 +1,25 @@
-"""
-Tic Tac Toe game definition
-
-Defines a Tic Tac Toe game state object with an associated transition function,
-a legal moves function, a move function, and a terminal state detector.
-Includes unit tests to verify proper functionality.
-"""
 import copy
 
 
 class GameState(object):
-    """
-    Represents a Tic Tac Toe game.
-    The state consists of a 3x3 game board with each position occupied by:
-      ' ' (empty square)
-      'X' (X mark)
-      'O' (O mark)
-    as well as the following terminal states:
-      X won
-      O won
-      Tie
-    """
     def __init__(self):
         # Begin with an empty game board
-        self.board = [[' ', ' ', ' ', ' ', ' '],
-                      [' ', ' ', ' ', ' ', ' '],
-                      [' ', ' ', ' ', ' ', ' '],
-                      [' ', ' ', ' ', ' ', ' '],
-                      [' ', ' ', ' ', ' ', ' ']]
+        self.board = [[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']]
 
     # GameState needs to be hashable so that it can be used as a unique graph
     # node in NetworkX
@@ -40,20 +33,12 @@ class GameState(object):
         return hash(self.__key())
 
     def __str__(self):
-        """
-        Returns a string that is a visual representation of the game
-        state. Can be used to print the current game state of a game:
-          print(game.state)
-        will print a game board:
-          ~X~
-          O~~
-          ~~X
-        """
+
         output = ''
-        for row in range(5):
-            for col in range(5):
+        for row in range(16):
+            for col in range(16):
                 contents = self.board[row][col]
-                if col < 4:
+                if col < 15:
                     output += '{}'.format(contents)
                 else:
                     output += '{}\n'.format(contents)
@@ -68,8 +53,8 @@ class GameState(object):
         """
         num_X = 0
         num_O = 0
-        for row in range(5):
-            for col in range(5):
+        for row in range(16):
+            for col in range(16):
                 if self.board[row][col] == 'X':
                     num_X += 1
                 elif self.board[row][col] == 'O':
@@ -84,9 +69,9 @@ class GameState(object):
         Places a marker at the position (row, col). The marker placed is
         determined by whose turn it is, either 'X' or 'O'.
         """
-        print('Move: {} moves to ({}, {})'.format(self.turn(), row, col))
+        #print('Move: {} moves to ({}, {})'.format(self.turn(), row, col))
         self.board[row][col] = self.turn()
-        print('{}'.format(self))
+        #print('{}'.format(self))
 
     def legal_moves(self):
         """
@@ -103,8 +88,8 @@ class GameState(object):
             return []
 
         possible_moves = []
-        for row in range(5):
-            for col in range(5):
+        for row in range(16):
+            for col in range(16):
                 if self.board[row][col] == ' ':
                     possible_moves.append((row, col))
         return possible_moves
@@ -133,52 +118,65 @@ class GameState(object):
         return new_state
 
     def winner(self):
-        """
-        Checks if the game state is a terminal state.
-        :return: If it is not, returns None; if it is, returns 'X' or 'O'
-        indicating who is the winner; if it is a tie, returns 'Tie'
-        """
+
         for player in ['X', 'O']:
-            # Check for winning horizontal lines
-            for row in range(5):
-                accum = 0
-                for col in range(5):
-                    if self.board[row][col] == player:
-                        accum += 1
-                if accum == 5:
-                    return player
+            # case -
+            for i in range(16):
+                for j in range(12):
+                    option = [ self.board[i][j+0],
+                               self.board[i][j+1],
+                               self.board[i][j+2],
+                               self.board[i][j+3],
+                               self.board[i][j+4]]
 
-            # Check for winning vertical lines
-            for col in range(5):
-                accum = 0
-                for row in range(5):
-                    if self.board[row][col] == player:
-                        accum += 1
-                if accum == 5:
-                    return player
+                    if all(marker == player for marker in option):
+                        return player
 
-            # Check for winning diagonal lines (there are 2 possibilities)
-            option1 = [self.board[0][0],
-                       self.board[1][1],
-                       self.board[2][2],
-                       self.board[3][3],
-                       self.board[4][4]]
-            option2 = [self.board[4][0],
-                       self.board[3][1],
-                       self.board[2][2],
-                       self.board[1][3],
-                       self.board[0][4]]
-            if all(marker == player for marker in option1) \
-                    or all(marker == player for marker in option2):
-                return player
+            # case |
+            for i in range(16):
+                for j in range(12):
+                    option = [self.board[j+0][i],
+                              self.board[j+1][i],
+                              self.board[j+2][i],
+                              self.board[j+3][i],
+                              self.board[j+4][i]]
+
+                    if all(marker == player for marker in option):
+                        return player
+
+            # case \
+            for i in range(12):
+                for j in range(12):
+                    option = [ self.board[i+0][j+0],
+                               self.board[i+1][j+1],
+                               self.board[i+2][j+2],
+                               self.board[i+3][j+3],
+                               self.board[i+4][j+4]]
+
+                    if all(marker == player for marker in option):
+                        return player
+
+            # case /
+            for i in range(12):
+                for j in range(12):
+                    option = [self.board[i+4][j+0],
+                              self.board[i+3][j+1],
+                              self.board[i+2][j+2],
+                              self.board[i+1][j+3],
+                              self.board[i+0][j+4]]
+
+                    if all(marker == player for marker in option):
+                        return player
+
+
 
         # Check for ties, defined as a board arrangement in which there are no
         # open board positions left and there are no winners (note that the
         # tie is not being detected ahead of time, as could potentially be
         # done)
         accum = 0
-        for row in range(5):
-            for col in range(5):
+        for row in range(16):
+            for col in range(16):
                 if self.board[row][col] == ' ':
                     accum += 1
         if accum == 0:
